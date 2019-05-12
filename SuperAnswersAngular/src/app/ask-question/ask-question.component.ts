fbbb07de5cf97d26170c0167d8cc026604fb375d
Michael Rosario <michael.p.rosario@gmail.com>
2019-05-12 17:40:31 -0400

    feat (ask question) implement ask question service

--- SuperAnswersAngular/src/app/ask-question/ask-question.component.ts
+++ SuperAnswersAngular/src/app/ask-question/ask-question.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionsService } from 'src/data-services/services';
+import { first } from 'rxjs/operators';
+import { AddQuestionRequest } from 'src/data-services/models';
 
 @Component({
   selector: 'app-ask-question',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AskQuestionComponent implements OnInit {
 
-  constructor() { }
+  private questionTitle: string;
+  private questionContent: string;
+  private questionTags: string;
+
+  constructor(
+    private questionsService: QuestionsService
+  ) { }
 
   ngOnInit() {
+  }
+
+  handleAskQuestion(){
+    let addQuestionRequest = {
+      questionTitle: this.questionTitle,
+      content: this.questionContent,
+      tags: this.questionTags,
+      notifyMeOnResponse: false
+    }
+
+    this.questionsService.QuestionsAddQuestion(addQuestionRequest)
+    .pipe(first())
+    .subscribe(response => 
+    { 
+        console.log(response);
+    });
+
+    
+    console.log("saved.")
   }
 
 }
