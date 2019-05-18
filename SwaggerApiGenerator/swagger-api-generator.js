afef99f17a014d7038403c405252aef4b4291c0f
Michael Rosario <michael.p.rosario@gmail.com>
2019-05-18 01:48:35 -0400

    feat(code gen) Prototype code gen

--- SwaggerApiGenerator/swagger-api-generator.js
+++ SwaggerApiGenerator/swagger-api-generator.js
@@ -1,31 +1,76 @@
 var fs = require('fs');
 var Handlebars = require('handlebars');
 
-function fixEntityTypes(entity){
-    for(var property in entity.properties){
-        if(!entity.properties[property].type){
+function fixEntityTypes(entity) {
+    for (var property in entity.properties) {
+        if (!entity.properties[property].type) {
             entity.properties[property].type = 'any';
-        }else if(entity.properties[property].type==='array'){
+        } else if (entity.properties[property].type === 'array') {
             entity.properties[property].type = 'any';
-        }else if(entity.properties[property].type==='object'){
+        } else if (entity.properties[property].type === 'object') {
             entity.properties[property].type = 'any';
-        }else if(entity.properties[property].type==='integer'){
+        } else if (entity.properties[property].type === 'integer') {
             entity.properties[property].type = 'number';
         }
-    }    
+    }
 }
 
-function generateViewModelEntity(definitions,entity){
+function generateViewModelEntity(definitions, entity) {
     var templateString = fs.readFileSync('templates/entity.template', 'utf8');
     var template = Handlebars.compile(templateString);
     var entityObject = definitions[entity];
     fixEntityTypes(entityObject);
-
     entityObject.entityName = entity;
-    
     var templateOutput = template(entityObject);
+    templateOutput += "\n\n";
+    return templateOutput;
+}
 
-    console.log(templateOutput);
+function generateEndPoint(paths, path) {
+    var templateString = fs.readFileSync('templates/endpoint.template', 'utf8');
+    var template = Handlebars.compile(templateString);
+    var endPoint = paths[path];
+
+    endPoint.endPointName = path.split('/')[2];
+    endPoint.controllerName = path.split('/')[1];
+    var templateOutput = template(endPoint);
+    templateOutput += "\n\n";
+    return templateOutput;
+}
+
+function getServiceClassStart(controllerName) {
+    var templateString = fs.readFileSync('templates/service-class-start.template', 'utf8');
+    var template = Handlebars.compile(templateString);
+    var data = {};
+    data.controllerName = controllerName;
+    var templateOutput = template(data);
+    templateOutput += "\n\n";
+    return templateOutput;
+}
+
+function getServiceClassStart(controllerName) {
+    var templateString = fs.readFileSync('templates/service-class-start.template', 'utf8');
+    var template = Handlebars.compile(templateString);
+    var data = {};
+    data.controllerName = controllerName;
+    var templateOutput = template(data);
+    templateOutput += "\n\n";
+    return templateOutput;
+}
+
+function getServiceFileStart() {
+    var templateString = fs.readFileSync('templates/service-file-start.template', 'utf8');
+    templateString += "\n\n";
+    return templateString;
+}
+
+function getServiceClassEnd() {
+    var templateString = fs.readFileSync('templates/service-class-end.template', 'utf8');
+    var template = Handlebars.compile(templateString);
+    var data = {};
+    var templateOutput = template(data);
+    templateOutput += "\n\n";
+    return templateOutput;
 }
 
 fileName = 'swagger-api.json';
@@ -33,15 +78,22 @@ var swaggerApiString = fs.readFileSync(fileName, 'utf8');
 var swaggerJsonData = JSON.parse(swaggerApiString);
 var definitions = swaggerJsonData.definitions;
 
-for(var entity in definitions){
-    generateViewModelEntity(definitions,entity);
+var output = ""
+output += getServiceFileStart();
+
+for (var entity in definitions) {
+    output += generateViewModelEntity(definitions, entity);
 }
 
+var controllersToProcess = ['Questions']
 
-// generate models
-// generate requests and responses
-
-// generate service per controller
-
-    // generate end points for controller
-
+controllersToProcess.forEach(function(controller){
+    output += getServiceClassStart(controller)
+    for (var path in swaggerJsonData.paths) {
+        if(path.startsWith('/'+controller+'/')){
+            output += generateEndPoint(swaggerJsonData.paths, path);
+        }
+    }
+    output += getServiceClassEnd()    
+})
+console.log(output);
