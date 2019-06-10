62f5120e23d944565e1562895eeb282f6db962f9
Michael Rosario <michael.p.rosario@gmail.com>
2019-06-10 06:14:14 -0400

    fix ( app ) Correct config for deployment

--- SuperAnswersAngular/src/data-services/api-configuration.ts
+++ SuperAnswersAngular/src/data-services/api-configuration.ts
@@ -1,5 +1,6 @@
 /* tslint:disable */
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment'
 
 /**
  * Global configuration for Api services
@@ -8,7 +9,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = 'http://localhost:8080';
+  rootUrl: string = environment.apiUrl;
 }
 
 export interface ApiConfigurationInterface {
