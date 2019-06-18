9a3e2cb0432ea75d59514f34e761764485ee2250
Michael Rosario <michael.p.rosario@gmail.com>
2019-06-18 07:01:08 -0400

    Add temp debug code

---
+++ App/Controllers/MyDebugController.cs
@@ -0,0 +1,24 @@
+﻿using App.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace App.Controllers
+{
+    public class MyDebugController : Controller
+    {
+        private readonly IOptions<AppSettings> settings;
+
+        public MyDebugController(IOptions<AppSettings> settings)
+        {
+            this.settings = settings;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(settings.Value);
+        }
+    }
+}
